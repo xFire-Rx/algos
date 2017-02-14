7b1a23271886815a4ba6b7bf67b6e893d243e0e7
xFire-Rx <users.xFire-Rx@noreply.github.com>
2017-02-14 12:56:27 -0400

    Added bubble sort

--- sort.cpp
+++ sort.cpp
@@ -12,14 +12,13 @@ using namespace std;
 \param [in] data The data set that will be searched
 \returns location of key if found or -1 if not found
 */
-void bubbleSort(auto & data);//prototype
+void bubbleSort(auto&data);//prototype
 
 
 int main()
 {
   vector<string> inputs;
   string input;
-  //int result;
 
    cout<<"Welcome to \"sort it\". We first need some input data."<<endl;
    cout<<"We'll assume the inputs do not have any spaces."<<endl<<endl;
@@ -41,42 +40,22 @@ int main()
        exit(1);//nothing to do but quit program
   }
  
-for (unsigned int x = 0 ;x < inputs.size(); x++){
-	cout << inputs[x];
+for (int x=0;x<inputs.size();x++){
+	bubbleSort(inputs);
+	cout<<inputs[x] << endl;
 }
-   //cout<<endl<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
-  //cout<<"Enter a value: ";
-
-
-   /*cin>>search_key;
- 
-    while(search_key != "#")//perform searches until sentinel entered
-    {
-       
-
-        cout<<"  '"<<search_key<<"' was ";
-
-        if (result == -1)
-          cout<<"not found";
-        else
-          cout<<"found at index "<<result;
-
-
-        cout<<endl<<endl<<"Enter a value to search for: ";
-        cin>>search_key; 
-    }*/
-
-   cout<<endl<<"Program \"sort it\" is now finished."<<endl<<endl;
-   bubbleSort(inputs);
     return 0;
 }
 
-void bubbleSort(auto & data){//implementation
-
-for(unsigned int i = 0; i < data.size();i++){
-	for(unsigned int j = 0; j < data.size();j++){ 
-	if(data[j] == data[j+1]){
-	swap(data[j],data[j+1]);
+void bubbleSort(auto&data)
+{
+for(int i=0; i<data.size()-1;i++)
+{
+for(int j=0; j<data.size()-1;j++)
+{ 	
+if(data[j]>data[j+1])
+{
+swap(data[j],data[j+1]);
 }
 }
 }
